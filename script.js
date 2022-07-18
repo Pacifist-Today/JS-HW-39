1d26f12e0f7e22201b08e25346234a31a16858fc
Pacifist <desmondmorales92@gmail.com>
2022-07-18 19:45:50 +0300

    Set data structure

--- script.js
+++ script.js
@@ -2,6 +2,72 @@
 
 (function () {
 
+    let cache = new Set ()
+    let rewriteArr = []
+
+    function caching (arg, func) {
+        let count = 1
+        let temporaryArr = [arg, func, count]
+        let item
+
+
+        if (rewriteArr.length === 0) {
+            temporaryArr[2]--
+            // cache.add(temporaryArr)
+            rewriteArr.push(temporaryArr)
+        }
+        for (item of rewriteArr) {
+            if (item[0] === temporaryArr[0]) {
+                item[2]++
+                return
+            }
+        } if (item[0] !== temporaryArr[0] && rewriteArr.length < 10) {
+            rewriteArr.push(temporaryArr)
+            // cache.add(temporaryArr)
+        }
+        else if (rewriteArr.length < 11) {
+            rewriteArr.shift()
+            rewriteArr.push(temporaryArr)
+            rewriteArr.flat(2)
+            // cache.delete()
+            // cache.add(temporaryArr)
+        }
+    }
+
+    cache.add(rewriteArr)
+
+    console.log(rewriteArr)
+
+    console.log(cache)
+
+    let someFunc = function () {}
+
+    caching(1, someFunc)
+    caching(1, someFunc)
+    caching(2, someFunc)
+    caching(2, someFunc)
+    caching(2, someFunc)
+    caching(3, someFunc)
+    caching(4, someFunc)
+    caching(4, someFunc)
+    caching(5, someFunc)
+    caching(6, someFunc)
+    caching(6, someFunc)
+    caching(6, someFunc)
+    caching(7, someFunc)
+    caching(7, someFunc)
+    caching(7, someFunc)
+    caching(7, someFunc)
+    caching(8, someFunc)
+    caching(8, someFunc)
+    caching(9, someFunc)
+    caching(10, someFunc)
+    caching(10, someFunc)
+    caching(10, someFunc)
+    caching(11, someFunc)
+    caching(12, someFunc)
+    caching(13, someFunc)
+
     // let cache = new Map ()
     // let rewriteArr = []
     // function caching (arg, func) {
@@ -51,68 +117,4 @@
     //         cache.set(temporaryArr, count)
     //     }
     // }
-
-    let cache = new Set ()
-    let rewriteArr = []
-
-    function caching (arg, func) {
-        let count = 1
-        let temporaryArr = [arg, func, count]
-        let item
-
-
-        if (rewriteArr.length === 0) {
-            temporaryArr[2]--
-            cache.add(temporaryArr)
-            rewriteArr.push(temporaryArr)
-        }
-        for (item of rewriteArr) {
-            if (item[0] === temporaryArr[0]) {
-                item[2]++
-                return
-            }
-        } if (item[0] !== temporaryArr[0] && rewriteArr.length < 10) {
-            rewriteArr.push(temporaryArr)
-            cache.add(temporaryArr)
-        }
-        else if (rewriteArr.length < 11) {
-            rewriteArr.shift()
-            rewriteArr.push(temporaryArr)
-            cache.add(temporaryArr)
-        }
-    }
-
-    // cache.add(rewriteArr)
-
-    console.log(rewriteArr)
-
-    console.log(cache)
-
-    let someFunc = function () {}
-
-    caching(1, someFunc)
-    caching(1, someFunc)
-    caching(2, someFunc)
-    caching(2, someFunc)
-    caching(2, someFunc)
-    caching(3, someFunc)
-    caching(4, someFunc)
-    caching(4, someFunc)
-    caching(5, someFunc)
-    caching(6, someFunc)
-    caching(6, someFunc)
-    caching(6, someFunc)
-    caching(7, someFunc)
-    caching(7, someFunc)
-    caching(7, someFunc)
-    caching(7, someFunc)
-    caching(8, someFunc)
-    caching(8, someFunc)
-    caching(9, someFunc)
-    caching(10, someFunc)
-    caching(10, someFunc)
-    caching(10, someFunc)
-    caching(11, someFunc)
-    caching(12, someFunc)
-    caching(13, someFunc)
 })()
